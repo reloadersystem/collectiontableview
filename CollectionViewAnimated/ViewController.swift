1587a77a70cffe064938c17d7537ef37182a5ed0
Reloader <suarezfc82@gmail.com>
2020-05-08 19:08:46 -0500

    Initial Commit

---
+++ CollectionViewAnimated/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewAnimated
+//
+//  Created by Resembrink Correa on 5/8/20.
+//  Copyright © 2020 Reloader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CollectionViewAnimatedTests/CollectionViewAnimatedTests.swift
@@ -0,0 +1,34 @@
+//
+//  CollectionViewAnimatedTests.swift
+//  CollectionViewAnimatedTests
+//
+//  Created by Resembrink Correa on 5/8/20.
+//  Copyright © 2020 Reloader. All rights reserved.
+//
+
+import XCTest
+@testable import CollectionViewAnimated
+
+class CollectionViewAnimatedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
